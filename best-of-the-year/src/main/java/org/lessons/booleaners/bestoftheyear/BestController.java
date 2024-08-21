6d34681b422c629cc82c11d465f979b43c5a2345
naracci.vincenzo <143946955+naraccivincenzo@users.noreply.github.com>
2024-08-21 23:09:02 +0200

    first part completed

--- best-of-the-year/src/main/java/org/lessons/booleaners/bestoftheyear/BestController.java
+++ best-of-the-year/src/main/java/org/lessons/booleaners/bestoftheyear/BestController.java
@@ -3,6 +3,7 @@ package org.lessons.booleaners.bestoftheyear;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
@@ -17,33 +18,53 @@ public class BestController {
         model.addAttribute("name", "Vincenzo");
         return "home";
     }
+
     @GetMapping("/movies")
     public String movies(Model model) {
         model.addAttribute("movies",getBestMovies());
         return "movies";
     }
+
     @GetMapping("/songs")
     public String songs(Model model) {
         model.addAttribute("songs",getBestSongs());
         return "songs";
     }
 
-    private ArrayList getBestMovies(){
-        ArrayList movies = new ArrayList();
-        Movies a = new Movies(1, "Burnout");
-        Movies b = new Movies(2, "Turn me on");
-        movies.add(a);
-        movies.add(b);
+    @GetMapping("/movies/{id}")
+    public String movies(@PathVariable int id, Model model) {
+        model.addAttribute("movies", id);
+        for (Movies movie : getBestMovies()){
+            if (movie.getId() == id){
+                model.addAttribute("title", movie.getTitle());
+            }
+        }
+        return "details";
+    }
+
+    @GetMapping("/songs/{id}")
+    public String songs(@PathVariable int id, Model model) {
+        model.addAttribute("songs", id);
+        for (Songs song : getBestSongs()){
+            if (song.getId() == id){
+                model.addAttribute("title", song.getTitle());
+            }
+        }
+        return "details";
+    }
+
+    private ArrayList<Movies> getBestMovies(){
+        ArrayList<Movies> movies = new ArrayList<>();
+        movies.add(new Movies(1, "Burnout"));
+        movies.add(new Movies(2, "Turn me on"));
         return movies;
     }
 
 
-    private ArrayList getBestSongs(){
-        ArrayList songs = new ArrayList();
-        Songs c = new Songs(1, "Thuderstruck");
-        Songs d = new Songs(2, "Thuder");
-        songs.add(c);
-        songs.add(d);
+    private ArrayList<Songs> getBestSongs(){
+        ArrayList<Songs> songs = new ArrayList<>();
+        songs.add(new Songs(1, "Thunderstruck"));
+        songs.add(new Songs(2, "Thunder"));
         return songs;
     }
 }
